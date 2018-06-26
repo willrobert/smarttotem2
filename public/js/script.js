b9061f5c30745a2dbe2d31a0e0a62ea48d4410cf
William <william.almeidarobert@gmail.com>
2018-06-26 06:01:13 -0400

    add facial recognition api

--- public/js/script.js
+++ public/js/script.js
@@ -128,4 +128,25 @@ socket.on('bot reply', function(replyText) {
   outputBot.textContent = replyText;  
 });
 
+function init(){
+  const video = document.getElementById('video')
+  const canvas = document.getElementById('canvas')
+  const context = canvas.getContext('2d')
+  const tracker = new tracking.ObjectTracker('face')
+  tracking.track('#video',tracker, {camera : true})
+  tracker.on('track', event => {
+      //console.log(event)
+      context.clearRect(0,0,canvas.width, canvas.height)
+      event.data.forEach(rect => {
+          context.strokeStyle = '#ff000'
+          context.lineWidth = 2
+          context.strokeRect(rect.x, rect.y, rect.width, rect.height)
+          socket.emit('chat message', 'oi');
+          return;
+      });
+      
+  })
+}
+window.onabort = init();
+
 
